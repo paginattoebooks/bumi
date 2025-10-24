785a2d342ea2006b337c49f4fb774e8d2ad2f9d9
paginattoebooks <paginattoebooks@gmail.com>
2025-10-24 12:16:34 -0300

    Delete src/components/index.tsx

--- src/components/index.tsx
+++
@@ -1,7 +0,0 @@
-export { default as AuthModal } from "./AuthModal";
-
-// stubs (opcional, só pra não quebrar imports já existentes)
-export const PaywallModal = () => null;
-export const CreatePostModal = () => null;
-export const EbookReaderModal = () => null;
-export const ReportModal = () => null;
